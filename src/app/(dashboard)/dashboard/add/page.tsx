2f308a4ef6c257c2726d5cd7c057c2883c64a906
kosta-rizov <kosta.rizov85@gmail.com>
2024-02-06 01:08:22 +0100

    feat: add Authentication and friend requests

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,13 @@
+import AddFriendButton from '@/components/addFriendButton'
+import { FC } from 'react'
+
+const Add = ({}) => {
+  return (
+    <main className='pt-8'>
+        <h1 className='font-bold text-5xl mb-8'>Add Friend</h1>
+        <AddFriendButton />
+    </main>
+  )
+}
+
+export default Add
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,15 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/auth"
+
 const Dashboard = async () => {
+
+  const session = await getServerSession(authOptions)
+  console.log("🚀 ~ Dashboard ~ session:", session)
 
   return (
     <div>
       dashboard
+      {JSON.stringify(session)} 
     </div>
   )
 }
