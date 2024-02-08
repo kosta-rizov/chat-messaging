f5cbe2b4f28de3ab89d177794caa6601eccd2a24
kosta-rizov <kosta.rizov85@gmail.com>
2024-02-08 18:38:24 +0100

    feat: add messaging to a friend

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,8 +1,11 @@
+import ChatInput from "@/components/chatInput";
+import Messages from "@/components/messages";
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { messageArraySchema } from "@/lib/validations/message";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import { FC } from "react";
 
@@ -23,12 +26,11 @@ const getChatMessages = async (chatId: string) => {
 
     const dbMessages = results.map((message) => JSON.parse(message) as Message);
 
-    const reverseDbMessages = dbMessages.reverse()
+    const reverseDbMessages = dbMessages.reverse();
 
-    const messages = messageArraySchema.parse(reverseDbMessages)
+    const messages = messageArraySchema.parse(reverseDbMessages);
 
-    return messages
-
+    return messages;
   } catch (error) {
     notFound();
   }
@@ -49,7 +51,47 @@ const Chat = async ({ params }: chatProps) => {
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
   const initialMessages = await getChatMessages(chatId);
 
-  return <div>{chatId}</div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]">
+      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative flex items-center space-x-4">
+
+          <div className="relative">
+            <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+              <Image
+                fill
+                referrerPolicy="no-referrer"
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className="rounded-full"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col leading-tight">
+            <div className="text-xl flex items-center">
+              <span className="text-gray-700 mr-3 font-semibold">
+                {chatPartner.name}
+              </span>
+            </div>
+            <span className="text-sm text-gray-600">
+              {chatPartner.email}
+            </span>
+          </div>
+
+        </div>
+      </div>
+
+      <Messages
+        chatId={chatId}
+        chatPartner={chatPartner}
+        sessionImg={session.user.image}
+        sessionId={session.user.id}
+        initialMessages={initialMessages}
+      />
+      <ChatInput chatId={chatId} chatPartner={chatPartner} />
+    </div>
+  );
 };
 
 export default Chat;
